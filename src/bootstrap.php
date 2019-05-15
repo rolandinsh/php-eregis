897c2a3815da32ec762751a99a2e9085799e1c63
Rolands <rolands@mediabox.lv>
2019-05-15 17:11:44 +0300

    s:: to eregis::

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-use Republa\EregShim\Eregis as s;
+use Republa\EregShim\Eregis as eregis;
 
 if (!function_exists('ereg')) {
     function ereg($pattern, $string, &$regs = null)
     {
         return func_num_args() === 2
-            ? s::ereg($pattern, $string)
-            : s::ereg($pattern, $string, $regs);
+            ? eregis::ereg($pattern, $string)
+            : eregis::ereg($pattern, $string, $regs);
     }
 }
 
@@ -15,35 +15,35 @@ if (!function_exists('eregi')) {
     function eregi($pattern, $string, &$regs = null)
     {
         return func_num_args() === 2
-            ? s::eregi($pattern, $string)
-            : s::eregi($pattern, $string, $regs);
+            ? eregis::eregi($pattern, $string)
+            : eregis::eregi($pattern, $string, $regs);
     }
 }
 
 if (!function_exists('ereg_replace')) {
     function ereg_replace($pattern , $replacement , $string)
     {
-        return s::ereg_replace($pattern , $replacement , $string);
+        return eregis::ereg_replace($pattern , $replacement , $string);
     }
 }
 
 if (!function_exists('eregi_replace')) {
     function eregi_replace($pattern , $replacement , $string)
     {
-        return s::eregi_replace($pattern , $replacement , $string);
+        return eregis::eregi_replace($pattern , $replacement , $string);
     }
 }
 
 if (!function_exists('split')) {
     function split($pattern, $string, $limit = -1)
     {
-        return s::split($pattern, $string, $limit);
+        return eregis::split($pattern, $string, $limit);
     }
 }
 
 if (!function_exists('spliti')) {
     function spliti($pattern, $string, $limit = -1)
     {
-        return s::spliti($pattern, $string, $limit);
+        return eregis::spliti($pattern, $string, $limit);
     }
 }
